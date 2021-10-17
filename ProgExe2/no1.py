8c019100f90bade1033206bf38ce97c5f646c16e
Carmen1104 <carmencongko11@gmail.com>
2021-10-17 17:43:08 +0700

    Update no1.py

--- ProgExe2/no1.py
+++ ProgExe2/no1.py
@@ -1,10 +1,12 @@
 import math
 
-mass = int(input("Enter the mass of the cart (in kg):"))
-force = int(input("Enter the force to push the cart (in N):"))
+m = int(input("Enter the mass of the cart (in kg):"))
+f = int(input("Enter the force to push the cart (in N):"))
+g = 9.8
 
-angle = force / (mass * 9.8)
+angle = f / m / g
 sine =math.asin(angle)
+sine =math.degrees(sine)
 
 print("The angle of the ramp is ", end ="")
-print ("{:,.1f}".format((math.degrees(sine))))
+print ("{:,.1f}".format(sine), "degrees")
