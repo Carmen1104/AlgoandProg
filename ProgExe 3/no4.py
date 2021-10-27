5bffbed72347d7c67c77ec58f97511605de8daff
Carmen1104 <carmencongko11@gmail.com>
2021-10-28 00:01:37 +0700

    Update no4.py

--- ProgExe 3/no4.py
+++ ProgExe 3/no4.py
@@ -1,11 +1,8 @@
-import fractions
-
 def calcNewHeight ():
     IWidth = float(input("Enter the current width: "))
     IHeight = float(input("Enter the current height: "))
-    ratio = fractions.fromfloat(IWidth,IHeight)
     DWidth = float(input("Enter the desired width: "))
-    Dheight = ratio * DWidth
-    print("The corresponding height is ", Dheight)
+    DHeight = ((IHeight * DWidth) / IWidth)
+    print("The corresponding height is ", DHeight)
 
 calcNewHeight()
